2451dffde6b6e9195b6fcf57db1a6696dc0329a0
Kirstie Whitaker <kw401@cam.ac.uk>
2019-09-02 17:44:05 +0200

    Update install.R

--- install.R
+++ install.R
@@ -1,8 +1,8 @@
-ggplot2
-VGAM
-nnet
-glamss
-DMwR
-MASS
-stargazer
-dyplr
+install.packages("ggplot2")
+install.packages("VGAM")
+install.packages("nnet")
+install.packages("glamss")
+install.packages("DMwR")
+install.packages("MASS")
+install.packages("stargazer")
+install.packages("dyplr")
